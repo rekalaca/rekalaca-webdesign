49d58d213a146082cbc28aa287d42238d635ddbe
Rékási László <rekalaca@gmail.com>
2023-04-27 19:19:13 +0200

    majdnem

--- mail.php
+++ mail.php
@@ -23,11 +23,11 @@ echo'
 
   <title>rekalaca webdesign | reszponzív, mobilbarát weboldalak készítése!</title>
   <meta name="description"
-    content="Reszponzív, mobilbarát Weboldalak tervezése és kivitelezése kedvező áron!/Nyíregyháza">
+      content="Reszponzív, mobilbarát Weboldalak tervezése és kivitelezése kedvező áron!/Nyíregyháza">
   <meta name="robots" content="index, folow">
   <link rel="canonical" href="https://www.rekalaca-webdesign.hu/">
   <meta name="keywords"
-    content="reszponzív, weboldalkészítés, webdesign, weblapkészítés, honlapkészítés, weblap karbantartás, Nyíregyháza,rekalaca webdesign">
+      content="reszponzív, weboldalkészítés, webdesign, weblapkészítés, honlapkészítés, weblap karbantartás, Nyíregyháza, rekalaca webdesign, olcsó, kedvező áron, megbízható">
 
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
@@ -39,78 +39,35 @@ echo'
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-    crossorigin="anonymous">
-    </script>
-  <script>
-    $(function () {
-      $("#header").load("header.html");
-      $("#footer").load("footer.html");
-    });
-  </script>
+  <link href="assets/css/style.css" rel="stylesheet">  
 
 </head>
 
 <body>
-  
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero">
-    <div class="hero-container">
-      <a href="index.html" class="hero-logo" data-aos="zoom-in"><img src="assets/img/newlogo/Rekalaca.png" alt=""
+  
+    <!-- ======= Thanks Section ======= -->
+  <section id="thanks">
+    <div class="thanks-container">
+      <a href="index.html" class="hero-logo"><img src="assets/img/newlogo/webdesign750.png" alt=""
           style="max-width: 300px;"></a>
-      <h1 data-aos="zoom-in">Köszönöm megkeresését!</h1>
-      <h2 data-aos="fade-up">Hamarosan felveszem Önnel a kapcsolatot!</h2>
-      <a data-aos="fade-up" href="index.html" class="btn-get-started scrollto">Vissza a Főoldalra!</a>
+      <h1 style="font-size: 28px; margin-top: 50px; color: white">Köszönöm érdeklődését!</h1>
+      <h2 style="font-weight: 700; color: white;">Hamarosan felveszem Önnel a kapcsolatot!</h2 style="font-weight: 600;">
+      <a href="index.html" class="btn-get-started scrollto">Vissza a Főoldalra!</a>
     </div>
   </section>
   <!-- End Hero -->
 
+  
 
-
-
-  <main id="main">
-
-
-  <!-- ======= Footer Section ======= -->
-  <div id="footer"></div>
-  <!-- ======= End Footer Section ======= -->
-
-</main><!-- End #main -->
-
-
-
-
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center upbutton"><i
-    class="bi bi-arrow-up-short"></i></a>
-
-
-
-<!-- Vendor JS Files -->
-<script src="assets/vendor/aos/aos.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-
-
-
+ 
 
 </body>
 
 </html>
-
 
 ';
 
